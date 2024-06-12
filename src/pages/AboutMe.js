74d42fb1104d80688242b201074476b5c69f39f2
christian7974 <ctropeano@uri.edu>
2024-06-12 17:11:34 -0400

    Animated About Me Section
    Animated the About Me sections on the About Me page

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -1,6 +1,28 @@
+import { motion } from "framer-motion";
+
 import Navigation from "../components/Navigation";
 import AboutMeSection from "../components/AboutMeSection";
 import { about_me_list } from "../assets/about_me_list";
+
+const containerVariants = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.4
+    }
+  }
+};
+
+const itemVariants = {
+  hidden: { opacity: 0 },
+  show: { opacity: 1,
+          transition: {
+            duration: 0.4,
+            ease: "easeInOut"
+          }
+   }
+};
 
 export default function AboutMePage() {
     return (
@@ -9,22 +31,29 @@ export default function AboutMePage() {
         <div className="mx-auto w-max">
           <h1 className="reg-header">Who Am I?</h1>
         </div>
-        <div className="grid grid-cols-3 grid-rows-2 gap-4 mx-4">
-          {about_me_list.map((item, index) => {
-            const isPhoto = index === 1;
-            const background = !isPhoto ? "bg-featured-project" : "";
+        <motion.div 
+          className="grid grid-cols-3 grid-rows-2 gap-4 mx-4"
+          variants={containerVariants}
+          initial="hidden"
+          animate="show"
+          >
+            {about_me_list.map((item, index) => {
+              const isPhoto = index === 1;
+              const background = !isPhoto ? "bg-featured-project" : "";
 
-            return (
-              <AboutMeSection
-                key={index}
-                title={item.title}
-                description={item.description}
-                image={item.image}
-                background={background}
-              />
-            );
-          })}
-        </div>
+              return (
+                <motion.div key={index} variants={itemVariants}>
+                  <AboutMeSection
+                    key={index}
+                    title={item.title}
+                    description={item.description}
+                    image={item.image}
+                    background={background}
+                  />
+                  </motion.div>
+              );
+            })}
+        </motion.div>
       </div>
     );
   }
