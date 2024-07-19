5e98275cffee63466560e2709612d889832c0af3
christian7974 <ctropeano@uri.edu>
2024-07-19 13:05:14 -0400

    Updated Color, Behavior of Radar Chart
    Updated the colors and behaviors of the radar chart.

--- src/components/SkillRadarChart.js
+++ src/components/SkillRadarChart.js
@@ -8,64 +8,95 @@ import skills from '../assets/skills_array';
 ChartJS.register(RadialLinearScale, PointElement, LineElement, Filler, Tooltip, Legend);
 
 const SkillRadarChart = () => {
+
+    const beginnerName = "Novice";
+    const intermediateName = "Intermediate";
+    const advancedName = "Advanced";
+
     const skillLabels = skills.map(skill => skill.name);
     const skillValues = skills.map(skill => skill.value);
-  const data = {
-    labels: skillLabels,
-    datasets: [
-      {
-        label: 'Proficiency Level',
-        data: skillValues, // 1=Novice, 2=Advanced, 3=Expert
-        backgroundColor: 'rgba(75, 192, 192, 0.2)',
-        borderColor: 'rgba(75, 192, 192, 1)',
-        borderWidth: 1
-      },
-    ]
-  };
+    const data = {
+        labels: skillLabels,
+        datasets: [
+        {
+            label: 'Proficiency Level',
+            data: skillValues, // 1=Novice, 2=Advanced, 3=Expert
+            backgroundColor: '#35979740', // color of the area
+            borderColor: '#359797', // border of the area
+            borderWidth: 1,
+            pointRadius: 8,
+            pointHoverRadius: 15,
+        },
+        ]
+    };
 
-  const options = {
+    const options = {
+    // events: [], // Disable hover effect
     scales: {
-      r: {
+        r: {
         angleLines: {
-          display: false
+            display: false
         },
         min: -1,
         max: 3,
         ticks: {
-          stepSize: 1,
-          callback: function(value) {
-            if (value === 1) return 'Novice';
-            if (value === 2) return 'Advanced';
-            if (value === 3) return 'Expert';
-          },
-          font: {
-            size: 20 // size of the novice/advanced labels
-        },
-        },
-        pointLabels: {
+            stepSize: 1,
+            callback: function(value) {
+            if (value === 1) return beginnerName;
+            if (value === 2) return intermediateName;
+            if (value === 3) return advancedName;
+            },
+            color: '#E2E2E2', // color of the novice/advanced
+            backdropColor: "rgba(10, 10, 10, 1)",
             font: {
-                size: 20 // size of the languages/skills
-            }
+            size: 24, // size of the novice/advanced labels
+        },
+        },
+        pointLabels: { // skills
+            color: '#E2E2E2', // color of the skills
+            font: {
+                size: 28, // size of the languages/skills
+            },
         },
         grid: {
-          color: 'rgba(0, 0, 0, 0.1)',
-          backgroundColor: '#ffffff' // Add this line to set the background color to white
+            color: '#707070', // color of the circular lines
+            lineWidth: 2, // Width of the grid lines
+            borderColor: 'rgba(0, 0, 0, 0.2)', // Color of the outer border
+            borderWidth: 1 // Width of the outer border
+        },
         }
-      }
     },
     plugins: {
-      legend: {
+        legend: {
         display: false // Hide the legend
-      }
+        },
+        tooltip: {
+        titleFont: {
+            size: 20
+        },
+        bodyFont: {
+            size: 16
+        },
+        // Convert numerical values to 
+        callbacks: {
+            label: function(context) {
+                const value = context.raw;
+                if (value === 1) return beginnerName;
+                if (value === 2) return intermediateName;
+                if (value === 3) return advancedName;
+            }
+        },
+        
+        }
     },
     maintainAspectRatio: false // Add this option to allow custom size
-  };
+    };
 
-  return (
-    <div style={{ width: '100%', height: '100%', backgroundColor: '#ffffff', padding: '20px' }}>
-      <Radar data={data} options={options} />
+    return (
+    <div style={{ width: '70%', height: '70%', margin: '0 auto' }}>
+        <Radar data={data} options={options} />
     </div>
-  );
+    );
 };
 
 export default SkillRadarChart;
--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -1,19 +1,13 @@
-import { Chart } from "chart.js";
-import { useRef } from "react";
-
 import Navigation from "../components/Navigation";
 import PageTitle from "../components/PageTitle";
 import SkillRadarChart from "../components/SkillRadarChart";
 
 export default function SkillsPage() {
-    const columnTitleStyles = "text-5xl font-bold";
-    const columnStyle = "w-[30%] flex flex-col items-center gap-y-2 rounded-md";
-
     return (
         <>
             <Navigation />
             <PageTitle titleContent="Skills" />
-                <SkillRadarChart />
+            <SkillRadarChart />
             {/* <div className="flex justify-around phone:flex-col phone:items-center">
                 <div className={`${columnStyle} bg-red-300`}>
                     <h1 className={columnTitleStyles}>Novice</h1>
@@ -25,7 +19,6 @@ export default function SkillsPage() {
                     <h1 className={columnTitleStyles}>Advanced</h1>
                 </div>
             </div> */}
-            
         </>
     )
 }
