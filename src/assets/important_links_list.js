a1f1baf82833cb3e54311df454c4efe708c871bf
christian7974 <ctropeano@uri.edu>
2025-01-01 10:51:31 -0500

    Added New Resume
    Added updated resume omitting the phone number.

--- src/assets/important_links_list.js
+++ src/assets/important_links_list.js
@@ -3,5 +3,5 @@
 export const importantLinks = [
     {linkTitle: "Github", link: "https://github.com/christian7974"},
     {linkTitle: "LinkedIn", link: "https://www.linkedin.com/in/christiantropeano/"},
-    {linkTitle: "Résumé", link: "https://docs.google.com/document/d/1QccD-QtopnuSShMNlUV7joSSHz1U6WgbwAS_eqQvGGQ/edit?usp=sharing"}
+    {linkTitle: "Résumé", link: "https://docs.google.com/document/d/1YBmK2mP8xmqG8b-M3AMGodu_aGVmJqA2TAZaO-_zJH4/edit?usp=sharing"}
 ];
